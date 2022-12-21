c6fa38b37da1b272a5f06b882699af1f15a65937
redshifttt <num@privatevoid.net>
2022-12-21 23:42:29 +0000

    refactor: start transplanting commands

---
+++ hercules/commands/ask.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import random
+
+class Ask(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ask")
+    async def ask(self, ctx, *args):
+        args = " ".join(args)
+        if not "or" in args:
+            return
+
+        fortune = args.split("or")
+        fortune = [f.strip() for f in fortune]
+        fortune = random.choice(fortune)
+
+        await ctx.reply(fortune)
