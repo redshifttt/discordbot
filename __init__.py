ca3138e0555bd34e47e3b6c6b099d4edbb99a68f
redshifttt <num@privatevoid.net>
2022-12-22 00:06:02 +0000

    commands(userinfo): transplant userinfo command

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import discord
 from discord.ext import commands
 
 import hercules.commands
-from hercules.commands import ask, serverinfo, avatar
+from hercules.commands import ask, serverinfo, avatar, userinfo
 import hercules.listeners
 
 intents = discord.Intents.all()
@@ -21,6 +21,7 @@ async def on_ready():
     await bot.add_cog(hercules.commands.ask.Ask(bot))
     await bot.add_cog(hercules.commands.serverinfo.ServerInfo(bot))
     await bot.add_cog(hercules.commands.avatar.Avatar(bot))
+    await bot.add_cog(hercules.commands.userinfo.UserInfo(bot))
 
     await bot.add_cog(hercules.commands.User(bot)) # Eeventually remove this and replace with all the commands
     await bot.add_cog(hercules.listeners.BotListeners(bot))
--- hercules/commands/__init__.py
+++ hercules/commands/__init__.py
@@ -13,58 +13,6 @@ class User(commands.Cog):
         self.bot = bot
         self.time_format = "%a, %b %d %Y %H:%M"
 
-    @commands.command(name="userinfo")
-    async def userinfo(self, ctx):
-        if not ctx.message.mentions:
-            user = ctx.author
-        else:
-            user = ctx.message.mentions[0]
-
-        current_guild = ctx.guild
-        guild_name = current_guild.name
-        guild_icon = current_guild.icon.url
-
-        username = user.name
-        user_id = user.id
-        mention = user.mention
-        status = user.raw_status
-
-        pfp = user.avatar.url
-
-        user_roles = user.roles
-        mention = user.mention
-        highest_role = user.top_role.name
-        permissions = user.guild_permissions
-
-        perms = []
-        if permissions.administrator:
-            perms = ['Administrator']
-        else:
-            for k, v in iter(permissions):
-                if v:
-                    perms.append(f"{k.title().replace('_', ' ')}")
-
-        perms = ", ".join(perms)
-
-        roles = len(user_roles)
-
-        embed_content = {
-            "title": username,
-            "fields": [
-                { "name": "Roles", "value": roles, "inline": False },
-                { "name": "Highest role", "value": highest_role, "inline": False },
-                { "name": "Permissions", "value": perms, "inline": False },
-                { "name": "Mention", "value": mention, "inline": False },
-            ]
-        }
-
-        embed = discord.Embed().from_dict(embed_content)
-        embed.set_thumbnail(url=pfp)
-        embed.set_author(name=current_guild, icon_url=guild_icon)
-        embed.set_footer(text=f"ID: {user_id}")
-
-        await ctx.reply(embed=embed)
-
     @commands.command(name="search")
     async def search(self, ctx, *args):
         async with self.bot.get_channel(ctx.channel.id).typing():
