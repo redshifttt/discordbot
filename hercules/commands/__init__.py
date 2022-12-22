f7431be63afe12bb8480117727af9e33cb47d165
redshifttt <num@privatevoid.net>
2022-12-22 00:19:22 +0000

    commands(christmas): transplant christmas command

--- hercules/commands/__init__.py
+++ hercules/commands/__init__.py
@@ -13,13 +13,3 @@ class User(commands.Cog):
         self.bot = bot
         self.time_format = "%a, %b %d %Y %H:%M"
 
-    @commands.command(name="christmas")
-    async def christmas(self, ctx):
-        christmas = humanize.precisedelta(dt.datetime(2022, 12, 25, 0, 0, 0, 0) - dt.datetime.today(), minimum_unit="hours", format="%.0f")
-        if dt.datetime.today().month == 12:
-            reply = f"{christmas} till Christmas!"
-        else:
-            reply = "it's already christmas dummy!"
-
-        await ctx.reply(reply)
-
