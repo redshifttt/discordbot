0f38e4221ee64c0f2c24bff0f76d83424cf6fde8
redshifttt <num@privatevoid.net>
2022-12-27 21:55:54 +0000

    commands(serverinfo): refactor and simplify

--- hercules/commands/serverinfo.py
+++ hercules/commands/serverinfo.py
@@ -35,51 +35,29 @@ class ServerInfo(commands.Cog):
 
         roles = len(guild.roles)
 
-        channels = str(len(guild.channels))
+        channel_count = str(len(guild.channels))
         channel_type = {}
 
-        for channel in guild.channels:
-            type_of_channel = channel.type[0]
-            if type_of_channel not in channel_type:
-                channel_type[type_of_channel] = 1
-            else:
-                channel_type[type_of_channel] += 1
+        type_of_channel = [channel.type[0] for channel in guild.channels]
 
-        channels += f" ({', '.join([f'{v} {k}' for k,v in channel_type.items()])})"
+        for channel in type_of_channel:
+            if channel not in channel_type:
+                channel_type[channel] = 1
+            else:
+                channel_type[channel] += 1
+
+        channel_count = f"**{channel_count}**\n"
+        channel_count += "\n".join([f'*{v} {k}*' for k,v in channel_type.items()])
 
         embed_content = {
             "title": server_name,
             "fields": [
-                {
-                    "name": "Members",
-                    "value": members,
-                    "inline": False
-                },
-                {
-                    "name": "Owner",
-                    "value": f"{server_owner}",
-                    "inline": False
-                },
-                {
-                    "name": "Created",
-                    "value": f"{creation_date}",
-                    "inline": False
-                },
-                {
-                    "name": "Roles",
-                    "value": roles,
-                    "inline": False
-                },
-                {
-                    "name": "Channels",
-                    "value": channels,
-                    "inline": False
-                },
-                {
-                    "name": "Emotes",
-                    "value": emote_count,
-                    "inline": False
-                }
+                { "name": "Members", "value": members, "inline": False },
+                { "name": "Owner", "value": f"{server_owner}", "inline": False },
+                { "name": "Created", "value": f"{creation_date}", "inline": False },
+                { "name": "Roles", "value": roles, "inline": False },
+                { "name": "Channels", "value": channel_count, "inline": False },
+                { "name": "Emotes", "value": emote_count, "inline": False }
             ]
         }
 
