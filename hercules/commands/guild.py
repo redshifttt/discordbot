071e8fc09e82f1000552dcd9756ec1a082c7b275
redshifttt <num@privatevoid.net>
2023-04-20 11:35:43 +0100

    commands(guild): actually the id too lmao

--- hercules/commands/guild.py
+++ hercules/commands/guild.py
@@ -95,7 +95,7 @@ class GuildInfo(commands.Cog):
             await ctx.reply(":x: Guild not found.")
             return
 
-        guild_data = await GuildInfo(self.bot).get_guild_data(guild)
+        guild_data = await GuildInfo(self.bot).get_guild_data(guild.id)
 
         await ctx.reply(embed=guild_data)
 
