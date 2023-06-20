2d98456cdcbcec394faed44ee0aa36e8144f5b1b
redshifttt <num@privatevoid.net>
2023-06-20 15:31:30 +0100

    flake.nix: remove shellHook plus reformat

--- flake.nix
+++ flake.nix
@@ -1,26 +1,21 @@
 {
-    description = "Hercules Discord Bot";
+  description = "Hercules Discord Bot";
 
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-    outputs = { self, nixpkgs }@inputs:
-        let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-        devShells.x86_64-linux.default =
-            pkgs.mkShell {
-                packages = with pkgs; [
-                    python311
-                    python311Packages.discordpy
-                    python311Packages.requests
-                    python311Packages.aiohttp
-                    python311Packages.humanize
-                ];
-                shellHook = ''
-                    echo "devshell for the Hercules Discord bot"
-                    export PS1="(devshell) $PS1"
-                    '';
-            };
+      devShells.x86_64-linux.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python311
+            python311Packages.discordpy
+            python311Packages.requests
+            python311Packages.aiohttp
+            python311Packages.humanize
+          ];
+        };
     };
 }
