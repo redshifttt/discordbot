458570868e70fe361c1a9fcc09a5c44de142bd2b
redshifttt <num@privatevoid.net>
2023-07-02 23:47:13 +0100

    add python-lsp-server to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   description = "Hercules Discord Bot";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
 
   outputs = { self, nixpkgs } @ inputs:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
+    in {
       devShells.x86_64-linux.default = pkgs.mkShell {
           packages = with pkgs; [
             python311
@@ -15,6 +16,7 @@
             python311Packages.requests
             python311Packages.aiohttp
             python311Packages.humanize
+            python311Packages.python-lsp-server
           ];
         };
     };
