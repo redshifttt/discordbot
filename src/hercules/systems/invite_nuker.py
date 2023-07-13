8b6694fa86642a0428e6fd74a98f5c56a01b2c8b
redshifttt <num@privatevoid.net>
2023-07-13 13:33:26 +0100

    systems/invite_nuker: if the user is an admin don't delete the message

--- src/hercules/systems/invite_nuker.py
+++ src/hercules/systems/invite_nuker.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import sqlite3
-import hercules.helper.log as log
 import hercules.helper.herculesdb as db
 import datetime as dt
 
@@ -11,21 +10,18 @@ class InviteNuker(commands.Cog):
 
     @commands.Cog.listener(name='on_message')
     async def invite_nuker(self, message):
-        guild_id = message.guild.id
-
         db_connection, db_cursor = db.connect_to_db("data.db")
-
-        row = db_cursor.execute("SELECT invite_nuker_system FROM servers WHERE guild_id = ?", (guild_id,)).fetchone()
-
+        row = db_cursor.execute("SELECT * FROM servers WHERE guild_id = ?", (message.guild.id,)).fetchone()
         invite_nuker_system = row["invite_nuker_system"]
-
         db_connection.close()
 
+        triggers = ["discord.gg", "discord.com/invite"]
+
         if invite_nuker_system == 1:
-            if "discord.gg" in message.content or "discord.com/invite" in message.content:
-                await message.delete()
-                await message.author.timeout(dt.timedelta(minutes=1), reason=f"Advertising in #{message.channel.name}")
+            for t in triggers:
+                if t in message.content and not message.author.guild_permissions.administrator:
+                    await message.delete()
+                    await message.author.timeout(dt.timedelta(minutes=1), reason=f"Advertising in #{message.channel.name}")
 
 async def setup(bot):
-    log.in_log("INFO", "listener_setup", "Invite Nuker system has been loaded")
     await bot.add_cog(InviteNuker(bot))
